3da282c5b550ea6f9ffe8486a306aaf075bf34cd
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-01-25 17:53:49 +0000

    add peers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi')
 
 const defaults = {}
 module.exports = ({ schema, options}) => {
-  const options = { ...defaults, ...options}
+  const settings = { ...defaults, ...options}
   return {
     before(handler, next) {
       return next()
